95f9d9eeb8eda3d47432637630a1c04265399b54
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-13 17:52:19 +0200

    Pages controller done. Navbar done skeleton

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def preload
+
+    redirect_to :home
+  end
+
+  def home
+
+  end
+
+  def about
+  end
+
+  def events
+  end
+  def gallery
+  end
+  def contact
+  end
+end
