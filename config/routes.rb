90b38f04d1464d45ad8b98c528eecb83b9c03510
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-25 17:59:28 +0200

    Form showing and executing order, error when submiting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'contact/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
-  get 'contact', to: 'pages#contact'
+  #get 'contact', to: 'pages#contact'
   get 'events', to: 'pages#events'
 
-
+resources :contacts, only: [:new, :create]
 end
