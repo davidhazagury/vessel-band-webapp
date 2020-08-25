222655d422303ee1203376c6fb9444692e9b9cdf
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-25 13:34:33 +0200

    Form added to contact page. Not sending emails

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
-  get 'contact', to: 'pages#contact'
+  #get 'contact', to: 'pages#contact'
   get 'events', to: 'pages#events'
 
-  resources :contacts, only: [:create]
+  resources :contact, only: [:index, :new, :create]
+
 end
