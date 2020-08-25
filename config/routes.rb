165cd1b79fb35b331b1e33d41a053833b6df101d
David Hernandez Azagury <davidhazagury@gmail.com>
2020-08-25 17:42:39 +0200

    Restarting contact-form

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
-  #get 'contact', to: 'pages#contact'
+  get 'contact', to: 'pages#contact'
   get 'events', to: 'pages#events'
 
-  resources :contact, only: [:index, :new, :create]
 
 end
